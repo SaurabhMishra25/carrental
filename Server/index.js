14a2d1ef7c8b9d53caaada0b41f7a07fb5f08abb
saurabhmishra <210120107509@git.org.in>
2024-05-02 13:46:48 +0530

    2may

--- Server/index.js
+++ Server/index.js
@@ -9,20 +9,20 @@ app.use(cors())
 //http://localhost:1000
 app.get('/',(req,res)=>{  res.send("hello from server")})
 //http://localhost:1000/create-payment-intent
-app.post("/create-payment-intent", async (req, res) => {
+app.post("/create-payment-intent", async (req, res) => {    
   const { amount,details } = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
     amount:amount * 100,
     currency: "usd",
-    description:"car rental",
+    // description:"car rental",
     automatic_payment_methods: {
       enabled: true,},
-      shipping:{
-        address:{
-            line1:"aaa", line2:"aaa",
-            city:"Ahmedabad", state:"Gujrat",
-            country:"US" },
-        name:details.name,phone:details.mobile  }
+      // shipping:{
+      //   address:{
+      //       line1:"aaa", line2:"aaa",
+      //       city:"Ahmedabad", state:"Gujrat",
+      //       country:"US" },
+      //   name:details.name,phone:details.mobile  }
 })
   res.send({
     clientSecret: paymentIntent.client_secret});
